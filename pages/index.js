cc629a173f2b6a55b4ab88d5330e2d17302b50ef
NikunjThesiya <nikunjthesiya.js@gmail.com>
2022-07-19 23:41:01 +0530

    quiz screen added

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,182 @@
 import Head from "next/head"
-import Image from "next/image"
-import illustrator from "../assets/images/login.svg"
-import facebook from "../assets/images/facebook.svg"
-import apple from "../assets/images/apple.svg"
-import google from "../assets/images/google.svg"
-import SocialLink from "../components/SocialLink"
-import styles from "../styles/Home.module.css"
-import Input from "../components/Input"
-import { MdOutlineAlternateEmail, MdOutlineStore } from "react-icons/md"
-import { FiLock } from "react-icons/fi"
-import { AiOutlineUser } from "react-icons/ai"
+import { BiArrowBack } from "react-icons/bi"
 import Link from "next/link"
+import { useEffect, useState } from "react"
+import ScrollContainer from "react-indiana-drag-scroll"
+
+// Quiz Data
+const QuizQue = [
+	{
+		id: 1,
+		question: "What is the name of the AngularJS framework?",
+		answer: "Angular",
+		options: ["Angular", "AngularJS", "Angular 2", "Angular 4"],
+		optionsType: "big",
+		image:
+			"https://images.unsplash.com/photo-1634581448750-22a591d78099?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80",
+	},
+	{
+		id: 2,
+		question: "What is the name of the React library?",
+		answer: "Angular",
+		optionsType: "small",
+		options: ["React", "React 1", "React 3", "React 4"],
+	},
+	{
+		id: 3,
+		question: "What is the name of the React Framework?",
+		answer: "Angular",
+		optionsType: "small",
+		options: ["Angular", "NextJS", "VueJS", "Ionic"],
+		image:
+			"https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1506&q=80",
+	},
+	{
+		id: 4,
+		question: "What is the name of the React library?",
+		answer: "Angular",
+		optionsType: "big",
+		options: ["React", "React 1", "React 3", "React 4"],
+	},
+	{
+		id: 5,
+		question: "What is the name of the AngularJS framework?",
+		answer: "Angular",
+		optionsType: "big",
+		options: ["Angular", "AngularJS", "Angular 2", "Angular 4"],
+		image:
+			"https://images.unsplash.com/photo-1634581448750-22a591d78099?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80",
+	},
+	{
+		id: 6,
+		question: "What is the name of the React library?",
+		answer: "Angular",
+		optionsType: "small",
+		options: ["React", "React 1", "React 3", "React 4"],
+	},
+	{
+		id: 7,
+		question: "What is the name of the React library?",
+		answer: "Angular",
+		optionsType: "big",
+		options: ["React", "React 1", "React 3", "React 4"],
+	},
+	{
+		id: 8,
+		question: "What is the name of the AngularJS framework?",
+		answer: "Angular",
+		optionsType: "big",
+		options: ["Angular", "AngularJS", "Angular 2", "Angular 4"],
+		image:
+			"https://images.unsplash.com/photo-1634581448750-22a591d78099?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80",
+	},
+]
+
+// Screen Header
+const Header = () => {
+	return (
+		<div className="w-full flex items-center justify-between">
+			<Link href="#">
+				<a>
+					<button className="btn-header rounded-r-full text-3xl">
+						<BiArrowBack />
+					</button>
+				</a>
+			</Link>
+			<div className="flex flex-col items-center text-white">
+				<span className="font-semibold">ANGULAR JS</span>
+				<span className="font-light text-xs">WORLD QUIZ</span>
+			</div>
+			<Link href="#">
+				<a>
+					<button className="btn-header rounded-l-full font-semibold text-base">
+						HB: W
+					</button>
+				</a>
+			</Link>
+		</div>
+	)
+}
+
+// View Component for Question
+const QuestionView = ({ totalQue, question }) => {
+	const [questionData, setQuestionData] = useState()
+	useEffect(() => {
+		totalQue.map((item) => {
+			item.id === question ? setQuestionData(item) : null
+		})
+	}, [question])
+
+	return (
+		<div className="w-full rounded-3xl flex flex-col gap-3 bg-white min-h-auto p-4">
+			{questionData?.image ? (
+				<div className="w-full h-48 flex bg-[#0BCE63] rounded-2xl overflow-hidden">
+					<img
+						src={questionData?.image}
+						alt="quiz"
+						className="w-full h-full object-cover"
+					/>
+				</div>
+			) : null}
+			<h3 className="uppercase text-[#858494] pl-1 font-normal">{`Question ${question} of ${totalQue?.length}`}</h3>
+			<h1 className="uppercase text-black pl-1 font-semibold">
+				{questionData?.question}
+			</h1>
+			<div
+				className={
+					questionData?.optionsType === "big"
+						? "grid grid-cols-1 gap-2"
+						: "grid grid-cols-2 gap-2"
+				}
+			>
+				{questionData?.options?.map((item, index) => (
+					<div
+						key={index}
+						className="flex w-full items-center justify-between cursor-pointer p-4 rounded-xl bg-opacity-30 border border-[#000000] border-opacity-10 hover:bg-[#0BCE63] hover:border-transparent hover:bg-opacity-30 transition ease-in-out duration-300"
+					>
+						{item}
+					</div>
+				))}
+			</div>
+			<button className="flex w-full items-center justify-center bg-[#0BCE63] p-4 text-white rounded-2xl text-xl text-center hover:bg-opacity-25 hover:text-[#0BCE63] transition ease-in-out duration-300 ">
+				Answer it
+			</button>
+		</div>
+	)
+}
 
 export default function Home() {
+	const [question, setQuestion] = useState(1)
 	return (
 		<div className="w-full bg-slate-100 flex items-center justify-center">
 			<Head>
-				<title>Sign Up Page</title>
+				<title>Quiz</title>
 				<meta name="description" content="Sign Up Page of NextMobile" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+			<div className="flex flex-col items-center w-full sm:w-[390px] min-h-screen bg-[#0BCE63] pb-6 py-4">
+				<Header />
 
-			<div className="flex flex-col items-center w-[390px] min-h-[844px] bg-white px-10 pb-6">
-				<Image src={illustrator} alt="sign up illustrator" />
-				<div className="flex items-start flex-col w-full gap-6">
-					<h1 className="text-blue-600 font-bold text-3xl">Sign Up</h1>
-					<div className="w-full grid grid-cols-3 gap-4">
-						<SocialLink
-							image={google}
-							alt="google logo"
-							link="www.google.com"
-						/>
-						<SocialLink
-							image={facebook}
-							alt="google logo"
-							link="www.facebook.com"
-						/>
-						<SocialLink image={apple} alt="apple logo" link="www.apple.com" />
-					</div>
-					<p className="text-sm text-gray-500 text-center self-center">
-						Or, register with email...
-					</p>
-					<form className="flex flex-col gap-6 w-full">
-						<div className="flex flex-col gap-4 w-full">
-							<Input
-								placeholder="Email ID"
-								icon={<MdOutlineAlternateEmail />}
-							/>
-							<Input placeholder="Password" icon={<FiLock />} />
-							<Input placeholder="Full Name" icon={<AiOutlineUser />} />
-							<Input placeholder="Company Name" icon={<MdOutlineStore />} />
+				<div className="flex flex-col gap-4 items-center w-full min-h-full bg-[#0BCE63] pb-6 px-3 py-4">
+					<ScrollContainer className="w-full flex items-center">
+						<div className="w-full flex gap-2 items-center justify-around">
+							{QuizQue.map((item) => (
+								<div
+									key={item.id}
+									className={
+										question === item.id
+											? "bg-white w-9 h-9 rounded-full text-sm font-medium flex items-center justify-center shadow-xl cursor-pointer"
+											: " w-9 h-9 rounded-full text-sm font-bold flex items-center justify-center cursor-pointer"
+									}
+									onClick={() => setQuestion(item.id)}
+								>
+									{item?.id}
+								</div>
+							))}
 						</div>
-						<button className="btn-primary">Sign Up</button>
-						<p className="text-gray-600 flex items-center gap-1 self-center text-sm">
-							<span>Already have an account?</span>
-							<span className="text-blue-600 hover:underline">
-								<Link href="/signin">Sign In</Link>
-							</span>
-						</p>
-					</form>
+					</ScrollContainer>
+
+					<QuestionView totalQue={QuizQue} question={question} />
 				</div>
 			</div>
 		</div>
